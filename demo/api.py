633640a3afcb771c6f839b10e10cf50ede4d837e
takapdayon <business.takap@gmail.com>
2025-01-29 12:25:29 +0900

    ブランチごとの変更差分確認

--- demo/api.py
+++ demo/api.py
@@ -1,8 +1,13 @@
-
 from ninja import Router
 
 router = Router()
 
-@router.get('/')
+
+@router.get("/")
 def test(request):
     return "test"
+
+
+@router.get("/change")
+def change(request):
+    return "change"
